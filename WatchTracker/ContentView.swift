4565bb21f4bd3c80b0690e57d303274d73eacfa7
faups <lukefaupel@gmail.com>
2019-06-28 20:40:27 -0500

    Initial Commit

---
+++ WatchTracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WatchTracker
+//
+//  Created by Luke Faupel on 6/28/19.
+//  Copyright © 2019 Luke Faupel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
